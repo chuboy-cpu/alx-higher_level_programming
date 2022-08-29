b02ec6675d96aa515873600c5a47413db64a4d79
Rivash <rivijerrier@gmail.com>
2022-08-29 15:27:39 -0700

    finds all the multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    mul = []
-    for x in range(len(my_list)):
-        if my_list[x] % 2 == 0:
-            mul.append(True)
-        else:
-            mul.append(False)
-        return mul
+    list = []
+    for x in my_list:
+        list.append(x %  2 == 0)
+    return list
