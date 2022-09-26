c8b475bcd900c0a6974fd09cbc26cf2e8ca258a8
rivash <rivijerrier@gmail.com>
2022-09-26 03:08:44 -0700

    returns attributes and methods of an object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,5 +4,5 @@
 
 
 def lookup(obj):
-    """ Returns the list of available attributes and methods of an object """
+    """ Returns the list of attributes and methods of an object """
     return dir(obj)
