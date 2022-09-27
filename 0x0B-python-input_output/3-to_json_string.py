a80b6e6def0ca3838ff758e0279134ac9f50050f
rivash <rivijerrier@gmail.com>
2022-09-27 04:07:02 -0700

    to JSON string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" Defines a function that returns the JSON representation """
+
+
+def to_json_string(my_obj):
+    """ return JSON representation of an object """
+    return json.dumps(my_obj)
