6e68b14f12a2c4b784c13e346e0a69805fd49a13
rivash <rivijerrier@gmail.com>
2022-11-02 05:46:58 -0700

    lists all privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all privilegers of the MySQL users
-SHOW GRANT FOR user_0d_1@localhost;
-SHOW GRANT FOR user_0d_2@localhost;
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
