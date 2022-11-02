ede8bbbf545fb8e5836f9cf8a955a1fb00f014cc
rivash <rivijerrier@gmail.com>
2022-11-01 17:29:30 -0700

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,10 @@
 -- creates a table and inserts data
 
 -- with attributes id, name, score
-CREATE TABLE IF NOT EXISTS second_table(id, INT, name VARCHAR(256), score INT);
+
+-- without using SELECT and SHOW statements
+
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table VALUES(1, 'John', 10);
 INSERT INTO second_table VALUES(2, 'Alex', 3);
 INSERT INTO second_table VALUES(3, 'Bob', 14);
