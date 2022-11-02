4d531a3b3a0863dbb7e40791c0056dad87a2791a
rivash <rivijerrier@gmail.com>
2022-11-02 11:23:36 -0700

    creates the database hbtn_0d_usa and the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,12 +4,12 @@
 -- state_id INT, can’t be null and must be a FOREIGN KEY 
 -- that references to id of the states table
 -- name VARCHAR(256) can’t be null
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
-USE DATABASE hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
-	id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	state_d INT NOT NULL,
-	FOREIGN KEY(state_d) REFERENCES states(id),
-	name VARCHAR(256)
+	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	state_id INT NOT NULL,
+	FOREIGN KEY(state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
 );
