ceddbdeeca49789a7d2a1f0bae3d9ad39b1ec34f
rivash <rivijerrier@gmail.com>
2022-11-02 10:44:40 -0700

    no comedy night

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,7 +6,7 @@ WHERE tv_shows.id NOT IN
 (SELECT tv_shows.id
 FROM tv_shows
 INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show.id
+ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = "Comedy")
