ae8d8f20995c431819d49dfa8cd02f1a1c3e415d
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-01-12 15:12:08 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import sys
     result = 0
-    for x in range(1,len(sys.argv)):
-        result += int(sys.argv[x])
-    print("{:d}".format(result), end="")
+    for x in range(len(sys.argv) - 1):
+        result += int(sys.argv[x + 1])
+        print("{:d}".format(result))
