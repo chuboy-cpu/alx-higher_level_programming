0209a2993cd1465d301a066345c0a71a40fc9ad7
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-01-12 15:12:52 +0100

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     result = 0
     for x in range(len(sys.argv) - 1):
         result += int(sys.argv[x + 1])
-        print("{:d}".format(result))
+    print("{:d}".format(result))
