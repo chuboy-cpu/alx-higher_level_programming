f10706d283fe1a3237a8dd2008c9238f0015c7b6
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-01-12 11:21:55 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,10 @@ def fizzbuzz():
     for n in range(1, 101):
         if(n % 3 == 0 and n % 5 == 0):
             print("{}".format("FizzBuzz"))
-        if (n % 3 == 0):
-            print("{}".format("Fizz"), end=" ")
-        if(n % 5 == 0):
-            print("{}".format("Buzz"), end=" ")
+        elif (n % 3 == 0):
+            print("{}".format("Fizz"), end="")
+        elif(n % 5 == 0):
+            print("{}".format("Buzz"), end="")
         else:
-            print("{}".format(n), end=" ")
+            print("{}".format(n), end="")
+
