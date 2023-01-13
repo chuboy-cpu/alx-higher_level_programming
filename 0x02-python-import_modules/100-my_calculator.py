fdaec906f18fdec4c992414df381a0485aa70d0b
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-01-13 09:00:25 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>", end="")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     operations = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(operations.keys()):
-        print("Unknown operator. Availble operators: +, -, * and /", end="")
+        print("Unknown operator. Availble operators: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
