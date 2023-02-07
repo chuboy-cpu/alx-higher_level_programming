104a3cf4fd2ebfa06c724038ae8caa408db8eb49
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-02-07 10:59:15 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
     x = sorted(a_dictionary.values())
     if x:
         for k in a_dictionary:
