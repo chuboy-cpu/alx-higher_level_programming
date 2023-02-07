c1a6f7fc29842c9d0a2d10b746dd5d30c997ffb1
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-02-07 09:55:49 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    newlist = []
-    return (newlist.append(x) if x != search else newlist.append(replace) for x in my_list)
+    return [x if x != search else replace for x in my_list]
