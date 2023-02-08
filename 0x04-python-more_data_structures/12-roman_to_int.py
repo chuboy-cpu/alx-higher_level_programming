a361263a58895ac0db47a45ac8cb7bf8e079849c
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-02-08 08:28:26 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-
-def roman_to_int(roman_string):
+""" def roman_to_int(roman_string):
 	if roman_string is None or isinstance(roman_string, (int, float)):
 		return 0
 	roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
@@ -21,4 +19,14 @@ def roman_to_int(roman_string):
 		else:
 			sum += r_1
 			i += 1
-	return sum
+	return sum """
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    total = 0
+    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        arabic = digits[roman]
+        total += arabic if total < arabic * 5 else -arabic
+    return total
