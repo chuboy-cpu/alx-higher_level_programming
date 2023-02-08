574b6e1bb107e9aa4b953dc99df690695d155df3
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-02-08 08:12:55 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-	if roman_string is None or isinstance(roman_string, (int, float)):
+	if (roman_string is None) or (isinstance(roman_string, (int, float))):
 		return 0
 	roman = {"I":1, "V":5, "X":10, "L":50, "C":100,	"D":500, "M":1000}
 	sum = i = 0
