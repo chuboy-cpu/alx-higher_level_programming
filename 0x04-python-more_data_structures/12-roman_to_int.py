07ee46b2d24ed960835628929a084a7120a89eb1
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-02-08 04:05:00 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or roman_string.isdigit():
+    if roman_string is None or isinstance(roman_string, (int, float)):
         return None
     roman = {"I":1, "V":5, "X":10, "L":50, "C":100,	"D":500, "M":1000}
     sum ,i = 0, 0
@@ -9,12 +9,10 @@ def roman_to_int(roman_string):
     else:
         while i < len(roman_string):
             r_1 = roman[roman_string[i]]
-            print(r_1)
             if i != len(roman_string) - 1:
                 r_2 = roman[roman_string[i + 1]]
             else:
                 r_2 = 0
-            print(r_2)
             if r_2 > r_1:
                 sum += r_2 - r_1
                 i += 2
