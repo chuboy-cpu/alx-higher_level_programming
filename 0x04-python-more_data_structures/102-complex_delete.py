f6563dec2e00f1c0203115dc24bd0e53e79b2db9
Uzoma-Ezeuli Chukwuemeka <uzomaezeulic@gmail.com>
2023-02-22 09:31:13 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    remove =[]
-    for k in a_dictionary:
-        if value == a_dictionary[k]:
-            remove.append(k)
-    for i in remove:
-        del a_dictionary[i]
-    return a_dictionary 
-
-dict = {1:"we", 2:"do", 3:"pet", 4:"rat", 5:"pig", 6:"rat"}
+ remove =[]
+ for k in a_dictionary:
+  if value == a_dictionary[k]:
+   remove.append(k)
+ for i in remove:
+  del a_dictionary[i]
+ return a_dictionary 
